4b6f1aace77eee2ccea2df3a9f33f4116d737818
Josh Liuaana <jfliuaana@gmail.com>
2024-07-02 17:22:13 +0100

    update env variables

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC8tFgtohEr1TW1ZGkqSYWiz0CKdIufEVM',
-  authDomain: 'athena-5f808.firebaseapp.com',
-  projectId: 'athena-5f808',
-  storageBucket: 'athena-5f808.appspot.com',
-  messagingSenderId: '765099100744',
-  appId: '1:765099100744:web:ddfdfd9b4a187090e4a67d',
-  measurementId: 'G-HZ9R9FXYCZ',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
