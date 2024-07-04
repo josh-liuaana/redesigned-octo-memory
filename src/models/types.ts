c1f897a22e3405a5850c5b44eb9cba368e485555
Josh Liuaana <jfliuaana@gmail.com>
2024-07-04 17:15:32 +0100

    create placeholder data json for testing

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,8 +1,26 @@
 export interface BookData {
   name: string
   author: string
+  series: string
+  pages: number
 }
 
 export interface Book extends BookData {
   id: string
+}
+
+export interface Character extends CharacterData {
+  id: string
+}
+
+export interface CharacterData {
+  name: string
+  books: string[]
+  pageInfo: Details[]
+}
+
+export interface Details {
+  book: string
+  page: number
+  information: string
 }
